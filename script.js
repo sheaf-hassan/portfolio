610ec7202c226ee82df80f8aafaab606579b9a72
200-hassan <md.arhassan@gmail.com>
2026-08-22 15:19:57 +0600

    circular text spin add

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const sideMenu = document.querySelector('#sideMenu');
+const navBar = document.querySelector("nav");
+const navLinks = document.querySelector("nav ul");
+
 
 function openMenu(){
     sideMenu.style.transform = 'translateX(-16rem)';
@@ -7,3 +10,13 @@ function openMenu(){
 function closeMenu(){
     sideMenu.style.transform = 'translateX(16rem)';
 }
+
+window.addEventListener('scroll', ()=>{
+    if(scrollY > 50){
+        navBar.classList.add('bg-white/50', 'backdrop-blur-lg', 'shadow-sm');
+        navLinks.classList.remove('bg-white', 'shadow-sm');
+    }else{
+        navBar.classList.remove('bg-white/50', 'backdrop-blur-lg', 'shadow-sm');
+        navLinks.classList.add('bg-white', 'shadow-sm');
+    }
+})
