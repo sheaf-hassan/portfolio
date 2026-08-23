efde2a7393199ecc7e17dbe3ef6981597539c339
200-hassan <md.arhassan@gmail.com>
2026-08-23 16:06:27 +0600

    implement dark mode

--- script.js
+++ script.js
@@ -13,10 +13,23 @@ function closeMenu(){
 
 window.addEventListener('scroll', ()=>{
     if(scrollY > 50){
-        navBar.classList.add('bg-white/50', 'backdrop-blur-lg', 'shadow-sm');
-        navLinks.classList.remove('bg-white', 'shadow-sm');
+        navBar.classList.add('bg-white/50', 'backdrop-blur-lg', 'shadow-sm', 'dark:bg-[#11001f]', 'dark:shadow-white/20');
+        navLinks.classList.remove('bg-white', 'shadow-sm', 'dark:border', 'dark:border-white/70', 'dark:bg-transparent');
     }else{
-        navBar.classList.remove('bg-white/50', 'backdrop-blur-lg', 'shadow-sm');
-        navLinks.classList.add('bg-white', 'shadow-sm');
+        navBar.classList.remove('bg-white/50', 'backdrop-blur-lg', 'shadow-sm', 'dark:bg-[#11001f]', 'dark:shadow-white/20');
+        navLinks.classList.add('bg-white', 'shadow-sm', 'dark:border', 'dark:border-white/70', 'dark:bg-transparent');
     }
 })
+
+function toggleDarkMode() {
+    document.documentElement.classList.toggle("dark");
+
+    localStorage.theme =
+      document.documentElement.classList.contains("dark")
+        ? "dark"
+        : "light";
+  }
+
+  if (localStorage.theme === "dark") {
+    document.documentElement.classList.add("dark");
+  }
